bd6840a947afb97db713c1bc78d7045e0b012419
PraneethP328 <pabbathi.praneeth@iitgn.ac.in>
2025-08-26 23:51:51 +0530

    Add docstrings and trailing newline for Pylint

--- src/main.py
+++ src/main.py
@@ -1,9 +1,13 @@
+"""Simple stats calculator using mean, median, and standard deviation."""
+
 import math
 
 def mean(nums):
+    """Return the mean of a list of numbers."""
     return sum(nums) / len(nums)
 
 def median(nums):
+    """Return the median of a list of numbers."""
     nums.sort()
     n = len(nums)
     mid = n // 2
@@ -12,6 +16,7 @@ def median(nums):
     return nums[mid]
 
 def std_dev(nums):
+    """Return the standard deviation of a list of numbers."""
     m = mean(nums)
     return math.sqrt(sum((x - m) ** 2 for x in nums) / len(nums))
 
